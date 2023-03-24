4b03fbfdf6993640957f9850cad83469292dd12e
Benjamin Zipperer <tettettet@test.com>
2023-03-24 12:35:51 +0100

    Simple table

--- src/app/components/usertable/usertable.component.ts
+++ src/app/components/usertable/usertable.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs';    // Angular 15 >= so
-import { Of } from 'rxjs/add/observable/of';
+
 import {DataSource} from '@angular/cdk/collections';
 import { User } from '../../models/user.model';
 
@@ -16,7 +16,7 @@ export class UsertableComponent implements OnInit
   displayedColumns = ['name', 'email', 'phone', 'company'];
   constructor(private userService: UserService) 
   { 
-    Of('');
+   
   }
   
   ngOnInit() 
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable }   from '@angular/core';
 import { HttpClient }   from '@angular/common/http';
 import { Observable }   from 'rxjs';
-import 'rxjs/add/operator/map';
+
 import { User } from '../models/user.model';
 @Injectable()
 export class UserService {
